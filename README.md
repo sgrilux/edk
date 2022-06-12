27412ef3eb9fc884bc62d016fdfbf47d39d95e4b
Emiliano Sgrano <sgrilux@gmail.com>
2022-06-12 17:46:22 +0200

    add: encrypt and decrypt string

--- README.md
+++ README.md
@@ -1,7 +1,84 @@
 # edk
 Encrypt/Decrypt using AWS KMS
 
+## Create a KMS key via CLI
+
+* Create the key
+
+  ```bash
+  aws kms create-key \ 
+      --description "Default Key" \
+      --region eu-west-1
+  ``` 
+
+* Create an alias fro the key
+
+  ```bash
+  aws kms create-alias \
+      --alias-name alias/default_cmk \
+      --region eu-west-1
+  ```
+
+## Usage
 
 ```bash
-pipenv
+usage: edk.py [-h] -k <kms alias/id> {encrypt,decrypt} ...
+
+Encrypt/Decrypt a string or a file using AWS KMS
+
+options:
+  -h, --help            show this help message and exit
+  -k <kms alias/id>, --kms <kms alias/id>
+                        KMS alias or id
+
+commands:
+  {encrypt,decrypt}     command to run
+    encrypt             Encrypt a string or a file
+    decrypt             Decrypt a string or a file
+```
+
+**encrypt** and **decrypt** commands have the same arguments.
+
+### Encrypt
+
+```bash
+usage: edk.py encrypt [-h] [-s <string>] [-f <file>] [-o <output file>]
+
+options:
+  -h, --help            show this help message and exit
+  -s <string>, --string <string>
+                        String to encrypt/decrypt
+  -f <file>, --file <file>
+                        File to encrypt/decrypt
+  -o <output file>      Output encrypted string or file to a different filei (default: .encrypted)
+```
+
+### Decrypt
+
+```bash
+usage: edk.py decrypt [-h] [-s <string>] [-f <file>] [-o <output file>]
+
+options:
+  -h, --help            show this help message and exit
+  -s <string>, --string <string>
+                        String to encrypt/decrypt
+  -f <file>, --file <file>
+                        File to encrypt/decrypt
+  -o <output file>      Output encrypted string or file to a different filei (default: .encrypted)
+```
+
+## Examples
+
+I reccommend to use pipenv.
+
+* Install dependencies
+```bash
+# Install dependencies
+pipenv install
+```
+
+* Encrypt
+
+```bash
+pipenv run python src/edk.py -k "alias/default_cmk" encrypt -s "stringtoencrypt"
 ```
--- src/edk.py
+++ src/edk.py
@@ -1,6 +1,7 @@
 import argparse
-import logging
+import base64
 import json
+import logging
 
 import boto3
 from botocore.exceptions import ClientError
@@ -13,79 +14,119 @@ logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
-kms_client = boto3.client("kms", region_name=AWS_REGION)
-
 
 def parse_input_arguments():
 
     arg_parser = argparse.ArgumentParser(
         description="Encrypt/Decrypt a string or a file using AWS KMS")
 
-    subparser = arg_parser.add_subparsers(title="commands",
-                                          help="command to run",
-                                          required=True,
-                                          dest='command')
-    encrypt = subparser.add_parser(
-        'encrypt', help="Encrypt a string or a file")
-    decrypt = subparser.add_parser(
-        'decrypt', help="Decrypt a string or a file")
+    subparsers = arg_parser.add_subparsers(title="commands",
+                                           help="command to run",
+                                           required=True,
+                                           dest='command')
 
     arg_parser.add_argument("-k",
                             "--kms",
                             required=True,
+                            dest='kms',
                             metavar="<kms alias/id>",
                             help="KMS alias or id")
 
-    arg_parser.add_argument('-s',
-                            '--string',
-                            type=str,
-                            metavar="<string>",
-                            help="String to encrypt/decrypt")
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser.add_argument('-s',
+                               '--string',
+                               type=str,
+                               dest='string',
+                               metavar="<string>",
+                               help="String to encrypt/decrypt")
 
-    arg_parser.add_argument('-f',
-                            '--file',
-                            type=argparse.FileType('r'),
-                            metavar="<file>",
-                            help="File to encrypt/decrypt")
+    parent_parser.add_argument('-f',
+                               '--file',
+                               type=argparse.FileType('r'),
+                               dest='file',
+                               metavar="<file>",
+                               help="File to encrypt/decrypt")
 
-    arg_parser.add_argument('-o',
-                            type=argparse.FileType('w'),
-                            metavar="<output file>",
-                            help="Output encrypted string or file to a different filei (default: .encrypted)")
+    parent_parser.add_argument('-o',
+                               type=argparse.FileType('w'),
+                               dest='output',
+                               metavar="<output file>",
+                               help="Output encrypted string or file to a different filei (default: .encrypted)")
+
+    encrypt = subparsers.add_parser(
+        'encrypt',
+        parents=[parent_parser],
+        help="Encrypt a string or a file")
+    decrypt = subparsers.add_parser(
+        'decrypt',
+        parents=[parent_parser],
+        help="Decrypt a string or a file")
 
     args = arg_parser.parse_args()
 
     return args
 
 
-def encrypt_string(string, kms, output):
-    pass
+def encrypt_string(string, kms):
+    client = boto3.client("kms", region_name=AWS_REGION)
+
+    try:
+        cipher_text = client.encrypt(
+            KeyId=kms,
+            Plaintext=bytes(string, encoding='utf8')
+        )
+    except ClientError as err:
+        raise Exception(
+            "An error occurred during encryption of %s: %s", string, err)
+    else:
+        return base64.b64encode(cipher_text["CiphertextBlob"])
 
 
-def encrypt_file(file, kms, output):
-    pass
+def encrypt_file(file, kms):
+    return "Not implemented yet!"
 
 
-def decrypt_string(string, kms, output):
-    pass
+def decrypt_string(string, kms):
+    client = boto3.client("kms", region_name=AWS_REGION)
+
+    try:
+        plain_text = client.decrypt(
+            KeyId=kms,
+            CiphertextBlob=bytes(base64.b64decode(string))
+        )
+    except ClientError as err:
+        raise Exception(
+            "An error occurred during decryption of %s: %s", string, err)
+    else:
+        return plain_text["Plaintext"]
 
 
-def decrypt_file(file, kms, output):
-    pass
+def decrypt_file(file, kms):
+    return "Not implemented yet!"
 
 
 def main(args):
-    if args.command == 'encrypt':
-        if args.string:
-            encrypted = encrypt_string(args.string, args.kms, args.output)
-        if args.file:
-            encrypted = encrypt_file(args.file, args.kms, args.output)
+    try:
+        if args.command == 'encrypt':
+            encrypted = ""
+            if args.string:
+                encrypted = encrypt_string(args.string, args.kms)
+            if args.file:
+                encrypted = encrypt_file(args.file, args.kms)
 
-    if args.command == 'decrypt':
-        if args.string:
-            decrypted = decrypt_string(args.string, args.kms, args.output)
-        if args.file:
-            decrypted = decrypt_file(args.file, args.kms, args.output)
+            print(encrypted)
+
+        if args.command == 'decrypt':
+            decrypted = ""
+            if args.string:
+                decrypted = decrypt_string(
+                    args.string, args.kms).decode('utf8')
+            if args.file:
+                decrypted = decrypt_file(args.file, args.kms)
+
+            print(decrypted)
+    except Exception as ex:
+        logger.exception(ex)
 
 
 if __name__ == '__main__':
